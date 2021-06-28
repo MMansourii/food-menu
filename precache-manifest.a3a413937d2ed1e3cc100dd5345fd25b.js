7cc362be60be056f7fecb1c696682a60c0950660
Mohammad <57011800+Mammad10@users.noreply.github.com>
2021-06-28 08:56:13 +0300

    Updates

---
+++ precache-manifest.a3a413937d2ed1e3cc100dd5345fd25b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18fdf1b577479b6a54b451c070c7fa6d",
+    "url": "/food-menu/index.html"
+  },
+  {
+    "revision": "dee5235d3dfa825d4f24",
+    "url": "/food-menu/static/css/main.26e112f5.chunk.css"
+  },
+  {
+    "revision": "6e331cfd5d1a1283573a",
+    "url": "/food-menu/static/js/2.66b87b03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/food-menu/static/js/2.66b87b03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dee5235d3dfa825d4f24",
+    "url": "/food-menu/static/js/main.25d5c083.chunk.js"
+  },
+  {
+    "revision": "bcc95b75661111e1bfe2",
+    "url": "/food-menu/static/js/runtime-main.c80e59c0.js"
+  }
+]);
